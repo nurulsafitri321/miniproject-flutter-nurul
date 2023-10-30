1f27eee1da7b080fb4a99f02df874c7af8e281dc
Nurul Safitri <nrlsftr161@gmail.com>
2023-10-30 18:36:22 +0700

    mini project

--- applibrary/lib/screens/market/filters/search_bar.dart
+++ applibrary/lib/screens/market/filters/search_bar.dart
@@ -3,14 +3,17 @@ import 'package:http/http.dart' as http;
 
 class SearchBarSection extends StatelessWidget {
   const SearchBarSection({Key? key}) : super(key: key);
+  
 
   @override
+  
   Widget build(BuildContext context) {
     
     return TextField(
       onChanged: (value) {},
       decoration: InputDecoration(
-        hintText: 'Search...',
+        
+        hintText: 'Search...',     
         suffixIcon: const Icon(Icons.search),
         filled: true,
         fillColor: Colors.grey[50],
